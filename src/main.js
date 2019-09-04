a2f6580134e073228dacc6b0c86dc58055b47304
kira roberts and Loewy Malkovich <kira@>
2019-09-04 10:32:05 -0700

    add webpack, jasmine, karma dependencies, add basic file structure

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { pingPong } from './ping-pong'; //CHANGE TO CURRENT PROJECT/FILE NAMES
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
